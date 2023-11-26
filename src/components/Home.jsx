cb47a860d99fc563c4f32354f9c418e0a1042d35
hsmanik <hsmanik2000@gmail.com>
2023-11-26 17:11:28 +0530

    cards

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,3 +1,12 @@
 export const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className=" max-w-[1240px] grid grid-cols-3 mx-auto text-white">
+      <h1 className=" mx-auto">Card 1</h1>
+      <h1 className=" mx-auto">Card 2</h1>
+      <h1 className=" mx-auto">Card 3</h1>
+      <h1 className=" mx-auto">Card 4</h1>
+      <h1 className=" mx-auto">Card 5</h1>
+      <h1 className=" mx-auto">Card 6</h1>
+    </div>
+  );
 };
